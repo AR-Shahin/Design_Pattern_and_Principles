6c898d8fc7c95b8bda80f392d90e718e23c3e78b
AR Shahin <mdshahinmije96@gmail.com>
2021-08-27 20:13:22 +0600

    ISP

---
+++ SOLID/ISP.php
@@ -0,0 +1,52 @@
+<?php
+
+# Interface Segregation Principle
+// Interface == Contract
+// Segregation == Divide or Separate 
+// No client should be forced to depends on methods it doesn't use
+
+# Wrong Way
+
+interface File
+{
+    function parse();
+    function htmlContent();
+}
+
+
+class JsonFile implements File
+{
+    function parse()
+    {
+    }
+    function htmlContent()
+    {
+    }
+}
+
+
+# right way 
+
+interface File
+{
+    function parse();
+}
+
+interface Html
+{
+    function htmlContent();
+}
+
+class JsonFile implements File
+{
+    function parse()
+    {
+    }
+}
+
+class HtmlFile implements Html
+{
+    function htmlContent()
+    {
+    }
+}
