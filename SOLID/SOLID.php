2c2065486778564aff504860b9ec69c99423a5ec
AR Shahin <mdshahinmije96@gmail.com>
2021-08-28 09:43:16 +0600

    SOLID

---
+++ SOLID/SOLID.php
@@ -0,0 +1,27 @@
+<?php
+
+# SOLID Introduction Acronym 
+
+// SOLID principles are the design principles that enable us manage most of the software design problems
+
+// The term SOLID is an acronym for five design principles intended to make software designs more understandable, flexible and maintainable 
+
+// The principles are a subset of many principles promoted by Robert C Martin
+
+# If we don't follow SOLID Principles 
+
+// End up with tight or strong coupling of the code with many other modules/applications
+// Tight coupling caused time to implement any new requirement features or any bug fixes and some times it created unknowns issues
+// End up with a code which is not testable
+// End up with duplication of code 
+// End up creating new bugs by fixing another bug
+// End up with many unknown issues in the application development cycle.
+
+
+# If we follow SOLID principles 
+
+// Achieve reduction on complexity of code 
+// Increase readability, extensibility and maintenance 
+// Reduce error and implement Reusability
+// Achieve Better Testability 
+// Reduce tight coupling
